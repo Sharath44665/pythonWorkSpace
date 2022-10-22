ac9d822e0f46485dc0ff06df630c9d3bc6bb41af
Sharath44665 <sharathwelcomesyouin@gmail.com>
2022-10-22 19:02:39 +0530

    add quotes from api

--- kanye-quotes-start/main.py
+++ kanye-quotes-start/main.py
@@ -3,10 +3,12 @@ import requests
 
 def get_quote():
     #Write your code here.
-    pass
-
-
-
+    response = requests.get("https://api.kanye.rest/")
+    response.raise_for_status()
+    data = response.json()
+    # print(data)
+    data = data["quote"]
+    canvas.itemconfig(quote_text, text=data)
 
 
 window = Tk()
@@ -16,7 +18,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 15, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
