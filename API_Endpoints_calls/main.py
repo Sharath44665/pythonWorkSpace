4b9223a4fa10d55e776ddd061137e975fd2c11da
Sharath44665 <sharathwelcomesyouin@gmail.com>
2022-10-22 19:43:07 +0530

    get sunrise and sunset in hours

--- API_Endpoints_calls/main.py
+++ API_Endpoints_calls/main.py
@@ -1,7 +1,8 @@
 import requests
 import datetime as dt
-MY_LATITUDE=13.340881
-MY_LONGITUDE=74.742142
+
+MY_LATITUDE = 13.340881
+MY_LONGITUDE = 74.742142
 # response=requests.get("http://api.open-notify.org/iss-now.json")
 # response.raise_for_status()
 # # print(response)
@@ -14,21 +15,21 @@ MY_LONGITUDE=74.742142
 # iss_position=(latitude,longitude)
 # print(iss_position)
 
-# this is required for
-parameters={
-    "lat":MY_LATITUDE,
+# this is required for https://api.sunrise-sunset.org/json
+parameters = {
+    "lat": MY_LATITUDE,
     "lng": MY_LONGITUDE,
-    "formatted":0,
+    "formatted": 0,
 }
-response=requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-data=response.json()
+data = response.json()
 
-sunrise=data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset=data["results"]["sunset"].split("T")[1].split(":")[0]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 # print(data)
 # print(sunrise.split("T")[1].split(":")[0])
-timeNow=dt.datetime.now()
+timeNow = dt.datetime.now()
 print(sunrise)
 print(sunset)
 print(timeNow.hour)
