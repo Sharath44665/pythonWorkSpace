14076ec12f4ffbbda8acaddd2a33d59fbc6a34ae
Sharath44665 <sharathwelcomesyouin@gmail.com>
2022-10-28 20:08:27 +0530

    add user interface

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -1,2 +1,27 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.scoreLable = Label(text="Score: 0", foreground="white", background=THEME_COLOR)
+        self.scoreLable.grid(row=0, column=1)
+
+        self.canvas = Canvas(height=250, width=300, highlightthickness=0, background="white")
+        # self.canvas.config(background=THEME_COLOR)
+        self.questionText = self.canvas.create_text(125, 150, text="bleh bleh ", font=("Arial", 25, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        trueImage = PhotoImage(file="images/true.png")
+        self.trueButton = Button(image=trueImage)
+        self.trueButton.grid(row=2, column=0)
+
+        falseImage = PhotoImage(file="images/false.png")
+        self.falseButton = Button(image=falseImage)
+        self.falseButton.grid(row=2, column=1)
+        self.window.mainloop()
