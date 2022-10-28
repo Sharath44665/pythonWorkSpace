e458d8291f56ccacbac9889ef4cba455f8f41c65
Sharath44665 <sharathwelcomesyouin@gmail.com>
2022-10-28 20:24:14 +0530

    show Question text on ui

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -1,10 +1,12 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quizBrain: QuizBrain):
+        self.quiz=quizBrain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -14,7 +16,10 @@ class QuizInterface:
 
         self.canvas = Canvas(height=250, width=300, highlightthickness=0, background="white")
         # self.canvas.config(background=THEME_COLOR)
-        self.questionText = self.canvas.create_text(125, 150, text="bleh bleh ", font=("Arial", 25, "italic"))
+        self.questionText = self.canvas.create_text(125, 150,
+                                                    width=250,
+                                                    text="bleh bleh ",
+                                                    font=("Arial", 16, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         trueImage = PhotoImage(file="images/true.png")
@@ -24,4 +29,10 @@ class QuizInterface:
         falseImage = PhotoImage(file="images/false.png")
         self.falseButton = Button(image=falseImage)
         self.falseButton.grid(row=2, column=1)
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text=self.quiz.next_question()
+        self.canvas.itemconfig(self.questionText, text=q_text)
