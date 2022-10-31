81faf42c3a14e52ba60719e31a244ee22c4108ca
Sharath44665 <sharathwelcomesyouin@gmail.com>
2022-10-31 07:26:07 +0530

    check if it will rain for next 12 hours

--- rainAlert/main.py
+++ rainAlert/main.py
@@ -1,2 +1,27 @@
+import requests
 
-apiKey="344dcfaf1bc9eb7041ea7c5b07cc4b7d"
+apiKey=""
+api_url="https://api.openweathermap.org/data/2.8/onecall"
+# https://api.openweathermap.org/data/2.5/onecall?
+# https://api.openweathermap.org/data/2.5/onecall
+
+parameters={
+    "lat": 13.340881,
+    "lon": 74.742142,
+    "appid":apiKey,
+    "exclude": "current,minutely,daily"
+}
+response=requests.get(api_url,params=parameters)
+response.raise_for_status()
+
+weatherData=response.json()
+# print(weatherData)
+hourList=weatherData["hourly"]
+for idx in range(11):
+    hourValue=hourList[idx]
+    weatherValue=hourValue["weather"]
+    for val in weatherValue:
+        weatherID=val["id"]
+        if weatherID < 700:
+            print("bring umbrella")
+            break
