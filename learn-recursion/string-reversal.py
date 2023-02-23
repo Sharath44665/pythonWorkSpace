0861d68e32120617a4bfe99f218bbd525db0b6ca
Sharath44665 <sharathwelcomesyouin@gmail.com>
2023-02-23 20:26:28 +0530

    reverse string using slicing

--- learn-recursion/string-reversal.py
+++ learn-recursion/string-reversal.py
@@ -1,12 +1,29 @@
-def stringReversal(sentence):
-    length = len(sentence)
-    return stringReversalHelper(sentence, length)
-
-def stringReversalHelper(sentence, length, val=""):
-    if length == 0:
-        return val
-    val = val+sentence[length-1]
-    return stringReversalHelper(sentence, length-1, val)
-
+# def stringReversal(sentence):
+#     length = len(sentence)
+#     return stringReversalHelper(sentence, length)
+#
+# def stringReversalHelper(sentence, length, val=""):
+#     if length == 0:
+#         return val
+#     val = val+sentence[length-1]
+#     return stringReversalHelper(sentence, length-1, val)
+#
+#
 sentence = "the simple code"
-print(stringReversal(sentence))
+# print(stringReversal(sentence))
+
+
+
+def stringReversalHelper(sentence, val=""):
+    if sentence =="":
+        return val
+
+    val = sentence[0]+val
+    return stringReversalHelper(sentence[1:],val)
+
+# e  e+ ht
+# he  h+t
+# the  t
+
+
+print(stringReversalHelper(sentence))
