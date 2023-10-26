25c4009817be3a6f1bd2f37f7ef50041d4342700
Sharath44665 <sharathwelcomesyouin@gmail.com>
2023-10-26 20:28:07 +0530

    work on list comprehension

---
+++ list-comprehension/main.py
@@ -0,0 +1,23 @@
+myList = [1,2,3]
+newList = [n+1 for n in myList]
+print(newList) # [2, 3, 4]
+
+name = "Sharath"
+newName = [letter for letter in name ]
+print(newName) # ['S', 'h', 'a', 'r', 'a', 't', 'h']
+
+
+doubleList = [val*2 for val in range(1,5)]
+print(doubleList) # [2, 4, 6, 8]
+
+names = ["Ada", "Remy", "Kylen", "Alex", "Tristan", "Lian", "Jayleen","Trace"]
+# generate a list containing less than or equal to 4 letters of names
+namesOf4char = [newName for newName in names if len(newName) < 5]
+print(namesOf4char)
+
+# generate a list containing more than or equal to 5 letters
+# in their name and make it capital
+# example Tristan >= 5 , results = TRISTAN
+capsNamesOfFiveOrMore = [newName.upper() for newName in names if len(newName) >= 5 ]
+print(capsNamesOfFiveOrMore)
+
