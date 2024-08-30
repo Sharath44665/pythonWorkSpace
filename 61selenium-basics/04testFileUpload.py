8a88559efe0aaf566518afde76198ccdf845f4ec
Sharath44665 <sharathwelcomesyouin@gmail.com>
2024-08-30 22:02:57 +0530

    fix typo

--- 61selenium-basics/04testFileUpload.py
+++ 61selenium-basics/04testFileUpload.py
@@ -9,17 +9,16 @@ def testUploads():
     driver.get("https://the-internet.herokuapp.com/upload")
 
     # single dot. meaning currentWorking directory
-    uploadFile = os.path.abspath(os.path.join(os.path.dirname(__file__), ".", "selenium_4_logo.png  "))
-
-    print(uploadFile)
+    uploadFile = os.path.abspath(os.path.join(os.path.dirname(__file__), ".", "selenium_4_logo.png"))
 
     fileInput = driver.find_element(By.CSS_SELECTOR, value="input[type='file']")
     fileInput.send_keys(uploadFile)
-    driver.find_element(By.ID, value="file-upload").click()
+
+    driver.find_element(By.ID, value="file-submit").click()
 
     fileName = driver.find_element(By.ID, value="uploaded-files").text
 
-    assert fileName == "selenium-snapshot.png"
+    assert fileName == "selenium_4_logo.png"
 
     tearDown(driver)
     pass
